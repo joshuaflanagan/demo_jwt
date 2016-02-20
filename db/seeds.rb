cca10feaf86e1141345b60d905a0a8b9fc5d6178
Joshua Flanagan <joshuaflanagan@gmail.com>
2016-02-20 09:40:23 -0600

    Add example users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create! email: "alice@example.com", password: "password"
+User.create! email: "bob@example.com", password: "password"
