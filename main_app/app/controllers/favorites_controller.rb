e4285e7a9760cb1d804cb7001be43e5ab0d0a8fa
Joshua Flanagan <joshuaflanagan@gmail.com>
2016-02-21 12:36:52 -0600

    Identify current user to other apps, using JSON Web Token
    Very important that all apps that communicate via JWT
    share the `json_web_token_key` value in secrets.yml.

--- main_app/app/controllers/favorites_controller.rb
+++ main_app/app/controllers/favorites_controller.rb
@@ -2,4 +2,23 @@ class FavoritesController < ApplicationController
   def index
 
   end
+
+  private
+
+  def json_web_token
+      JWT.encode auth_payload, hmac_secret, 'HS256'
+  end
+  helper_method :json_web_token
+
+  def auth_payload
+    {
+      exp: 1.day.from_now.to_i, # https://tools.ietf.org/html/rfc7519#section-4.1.4
+      iat: Time.now.to_i,       # https://tools.ietf.org/html/rfc7519#section-4.1.6
+      user_id: current_user.id
+    }
+  end
+
+  def hmac_secret
+    Rails.application.secrets.json_web_token_key
+  end
 end
